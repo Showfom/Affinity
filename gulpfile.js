d8af9781c7e7abb237e6365baa4908945cd943d9
Jixun Moe <Jixun.Moe@Gmail.Com>
2017-05-16 00:51:27 +0100

    Fix gulp build file for new directory structure.

--- gulpfile.js
+++ gulpfile.js
@@ -10,29 +10,49 @@ var LessPluginAutoPrefix = require('less-plugin-autoprefix');
 
 var autoprefix= new LessPluginAutoPrefix({ browsers: ["last 4 versions"] });
 
-function move_header (file, enc, cb) {
-    if (file.isNull()) {
-      return cb(null, file);
-    }
+function PreserveLicense() {}
+PreserveLicense.prototype = {
+  save: function () {
+    var self = this;
+    return function (file, enc, cb) {
+      if (file.isNull()) {
+        return cb(null, file);
+      }
 
-    if (file.isStream()) {
-      return cb(createError(file, 'Streaming not supported', null));
-    }
+      if (file.isStream()) {
+        return cb(createError(file, 'Streaming not supported', null));
+      }
 
-	var licenses = [];
-	var withoutLicense = String(file.contents).replace(/\s+(\/\*\!.+?\*\/)\s+/g, function (z, license) {
-		licenses.push(license);
-		return '';
-	});
-	
-	if (licenses.length > 0) {
-		licenses.push('');
-	}
-	
-	file.contents = new Buffer(licenses.join('\n') + withoutLicense);
-	
-	cb(null, file);
-}
+      self.licenses = [];
+      String(file.contents).replace(/\s*(\/\*\!.+?\*\/)\s*/g, function (z, license) {
+        self.licenses.push(license);
+      });
+
+      cb(null, file);
+    }
+  },
+
+  restore: function () {
+    var self = this;
+    return function (file, enc, cb) {
+      if (file.isNull()) {
+        return cb(null, file);
+      }
+
+      if (file.isStream()) {
+        return cb(createError(file, 'Streaming not supported', null));
+      }
+
+      if (self.licenses.length > 0) {
+        self.licenses.push('');
+      }
+
+      file.contents = new Buffer(self.licenses.join('\n') + String(file.contents));
+
+      cb(null, file);
+    }
+  }
+};
 
 var assetsDir = './affinity/assets';
 
@@ -50,26 +70,36 @@ gulp.task('build', function() {
 });
 
 gulp.task('minify', function() {
+    var cssLicense = new PreserveLicense();
     gulp.src('./src/css/*.css')
         .pipe(concat('affinity.css'))
+        .pipe(through.obj(cssLicense.save()))
         .pipe(cleancss())
+        .pipe(through.obj(cssLicense.restore()))
         .pipe(rename({
             suffix: '.min'
         }))
-		.pipe(through.obj(move_header))
         .pipe(gulp.dest(assetsDir));
-    gulp.src('./src/js/**/*.js')
-        .pipe(uglify({
-			preserveComments: 'license',
-			properties: {
-				regex: /^m_/
-			}
-		}))
+
+    var jsLicense = new PreserveLicense();
+    gulp.src([
+    			'./src/js/lib/*.js',
+    			'./src/js/init.js',
+    			'./src/js/app/*.js',
+    		])
         .pipe(concat('affinity.js'))
+        .pipe(through.obj(jsLicense.save()))
+        .pipe(uglify({
+          mangle: {
+            properties: {
+              regex: /^m_/
+            }
+          }
+        }))
+        .pipe(through.obj(jsLicense.restore()))
         .pipe(rename({
             suffix: '.min'
         }))
-		.pipe(through.obj(move_header))
         .pipe(gulp.dest(assetsDir));
 });
 
