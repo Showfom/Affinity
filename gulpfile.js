d46ec738b5fba6839cf31231da18bfbcd176313e
Jixun Moe <Jixun.Moe@Gmail.Com>
2017-05-15 23:22:25 +0100

    Preserve library license comment

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,34 @@ var csscomb = require('gulp-csscomb');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var through = require('through2');
 var LessPluginAutoPrefix = require('less-plugin-autoprefix');
 
 var autoprefix= new LessPluginAutoPrefix({ browsers: ["last 4 versions"] });
+
+function move_header (file, enc, cb) {
+    if (file.isNull()) {
+      return cb(null, file);
+    }
+
+    if (file.isStream()) {
+      return cb(createError(file, 'Streaming not supported', null));
+    }
+
+	var licenses = [];
+	var withoutLicense = String(file.contents).replace(/\s+(\/\*\!.+?\*\/)\s+/g, function (z, license) {
+		licenses.push(license);
+		return '';
+	});
+	
+	if (licenses.length > 0) {
+		licenses.push('');
+	}
+	
+	file.contents = new Buffer(licenses.join('\n') + withoutLicense);
+	
+	cb(null, file);
+}
 
 gulp.task('watch', function() {
     gulp.watch('./**/*.less', ['build', 'minify']);
@@ -29,13 +54,20 @@ gulp.task('minify', function() {
         .pipe(rename({
             suffix: '.min'
         }))
+		.pipe(through.obj(move_header))
         .pipe(gulp.dest('./assets'));
-    gulp.src('./src/js/*.js')
+    gulp.src('./src/js/**/*.js')
+        .pipe(uglify({
+			preserveComments: 'license',
+			properties: {
+				regex: /^m_/
+			}
+		}))
         .pipe(concat('affinity.js'))
-        .pipe(uglify())
         .pipe(rename({
             suffix: '.min'
         }))
+		.pipe(through.obj(move_header))
         .pipe(gulp.dest('./assets'));
 });
 
